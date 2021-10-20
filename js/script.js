224c036945b91cf080aaba3642f17125d53d6799
AssasinDEAD <64725895+AssasinDEAD@users.noreply.github.com>
2021-10-20 14:36:38 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// for (let i = 1; i <= 1000; i++) {
+
+//    let string = String(i)
+
+//    if (string.includes('22')) {
+//       console.log(i)
+//    }
+// }
+
+for (let i = 0; i <= 100; i++) {
+   if (i ** 2 <= 100) {
+      console.log(i ** 2)
+   }
+}
